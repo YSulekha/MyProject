6001faf2d623c044a21a68b8975318dd365b0365
YSulekha <ysulekha@gmail.com>
2015-02-17 00:00:22 -0800

    S

---
+++ src/numden.java
@@ -0,0 +1,11 @@
+
+public class numden {
+	public static void main(String args[]){
+		numden nd = new numden();
+		nd.findnumbyden();
+	}
+	public void findnumbyden(){
+		float f = (float)1/3;
+		System.out.println(f);
+	}
+}
