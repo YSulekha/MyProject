05e657252a4516abc459dd0f42324c79d54f3fac
YSulekha <ysulekha@gmail.com>
2015-02-25 23:57:41 -0800

    Submit

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,26 +3,51 @@ public class QuickSort {
 	int num[];
 	public static void main(String args[]){
 		QuickSort q = new QuickSort();
-		q.randomNumberGen(9);
+		q.randomNumberGen(5);
 		q.print();
 		//m.merge = new int[m.num.length];
 		q.quicksort(q.num);
 		q.print();
 	}
 	public void quicksort(int[] quic){
-		int pivot = num[0];
+		int pivot = quic[0];
 		int i=1;
 		int j=quic.length-1;
+		int temp = 0;
+		int aux[];
 		while(i <=j){
-			if(quic[i] < pivot)
-				i++;
-			else if (quic[j] > pivot)
-				j++;
-			else{
-				
-			}
+			while(quic[i] < pivot)
+				i=i+1;
+			while(quic[j] > pivot)
+				j=j-1;
+			temp = quic[j];
+			quic[j] = quic[i];
+			quic[i] = temp;
 		}
-	}
+		if(j < i){
+			temp = quic[j];
+			quic[j] = quic[i];
+			quic[i] = temp;
+		}
+		temp = quic[j];
+		quic[j] = pivot;
+		quic[0] = temp;
+		aux = new int[i-0];
+		for(int k = 0;k < j;k++){
+			aux[k] = quic[k];
+		}
+		System.out.println("i "+i+"j "+j);
+		for(int z=0;z<aux.length;z++){
+			System.out.print(aux[z]+" ");
+		}
+		quicksort(aux);
+		aux = new int[j-i];
+		for(int k = j;k < quic.length;k++){
+			aux[k] = quic[k];
+		}
+		}
+	
+
 	public void randomNumberGen(int size){
 		num = new int[size];
 		for(int i=0;i<num.length;i++){
