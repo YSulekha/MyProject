6dbd97b5e74a56216c534e364bcc57e23fd66b2b
YSulekha <ysulekha@gmail.com>
2015-03-04 23:51:19 -0800

    Submitting

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class BinarySearch {
 	int nums[];
+	int searchitem;
 	public static void main(String[] args){
 		BinarySearch b = new BinarySearch();
 		b.userInput();
@@ -17,11 +18,22 @@ public class BinarySearch {
 		for(int i = 0;i<nums.length;i++){
 			nums[i] = s.nextInt();
 		}
+		System.out.println("enter the item to be searched");
+		searchitem = s.nextInt();
 	}
 	public int search(){
 		int low=0;
 		int high = nums.length-1;
 		int middle= (high-low)/2;
+		while(high>low){
+			if(searchitem > nums[middle])
+				low = middle+1;
+			else if(searchitem==nums[middle])
+				return 1;
+			else{
+				high = middle-1;
+			}
+		}
 		return 0;
 	}
 }
