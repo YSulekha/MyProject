f5049439547433b3bd2b5fc0cafdecded9638f80
YSulekha <ysulekha@gmail.com>
2015-03-10 23:52:45 -0700

    Submitting

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -9,6 +9,9 @@ public class InsertionSort {
 		System.out.println("Enter the size of array/n");
 		int size = s.nextInt();
 		i.randomNumberGen(size);
+		i.print();
+		i.sort();
+		i.print();
 	}
 	public void randomNumberGen(int size){
 		num = new int[size];
@@ -22,4 +25,22 @@ public class InsertionSort {
 		}
 		System.out.println();
 	}
+	public void sort(){	
+		int j=0;
+		for(int i=1;i<num.length;i++){
+			j = i;
+			while(j>=1 && num[j] < num[j-1]){
+					swap(j);
+					j--;
+				}
+			}
+			print();
+		}
+
+	public void swap(int k){
+		int l = 0;
+		l = num[k];
+		num[k] = num[k-1];
+		num[k-1] = l;
+		}
 }
