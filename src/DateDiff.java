315fffa2ae2d597c6438cc355efdbc39ed205c72
YSulekha <ysulekha@gmail.com>
2015-03-16 23:56:37 -0700

    Submitting

---
+++ src/DateDiff.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class DateDiff {
+	Date date1;
+	Date date2;
+	public static void main(String args[]){
+		DateDiff d = new DateDiff();
+		System.out.println("Enter the first date");
+		Scanner s = new Scanner(System.in);
+		String d1 = s.next();
+		System.out.println("Enter the second date");
+		String d2 = s.next();
+	}
+	public void getLeapYear(){
+		
+	}
+	public void getDateDiff(){
+		
+	}
+}
