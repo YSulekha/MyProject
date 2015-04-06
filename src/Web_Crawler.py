490a06cfa627910bb8ed736073ed90a9df6c2f6c
YSulekha <ysulekha@gmail.com>
2015-04-05 23:49:41 -0700

    Submitting

--- src/Web_Crawler.py
+++ src/Web_Crawler.py
@@ -7,7 +7,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-
-
-
+def web_crawler(max_pages):
+    pages=1
+    while pages <= max_pages:
+        from locale import str
+        url = 'https://www.thenewboston.com/forum/recent_activity.php?page='+ str(pages)
+        source_code = requests.get(url)
+        text = source_code.text
+        soup = BeautifulSoup(text)
+        for link in soup.findAll('a' , {'class':'post-title'}):
+            href = "https://www.thenewboston.com"+link.get('href')
+            print(href)
+        pages+=1
+        print(url)
+        
+web_crawler(1)
