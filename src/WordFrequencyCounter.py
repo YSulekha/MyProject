3a0a871518d91669b1983605f87600d2b10dcf18
YSulekha <ysulekha@gmail.com>
2015-04-11 23:52:05 -0700

    Submitting

--- src/WordFrequencyCounter.py
+++ src/WordFrequencyCounter.py
@@ -3,5 +3,10 @@ Created on Apr 7, 2015
 
 @author: Sulekha
 '''
-def wordCounter():
-    
+import requests
+from bs4 import BeautifulSoup
+
+def wordCounter(url):
+    source_code = requests.get(url)
+    text = source_code.text
+    soup = BeautifulSoup(text)
