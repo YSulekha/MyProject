d313fc2c1b8488aad16e8a6b2043af498b39c126
YSulekha <ysulekha@gmail.com>
2015-04-12 23:38:11 -0700

    Submitting

--- src/WordFrequencyCounter.py
+++ src/WordFrequencyCounter.py
@@ -7,6 +7,16 @@ import requests
 from bs4 import BeautifulSoup
 
 def wordCounter(url):
+    word_list = []
     source_code = requests.get(url)
     text = source_code.text
     soup = BeautifulSoup(text)
+    for post_text in soup.findAll('a' ,{'class':'post-title'}):
+        content = post_text.string
+        words = content.split()
+        for word in words:
+            print(word)
+            word_list.append(word)
+    
+wordCounter('https://www.thenewboston.com/forum/')
+        
