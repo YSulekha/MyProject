f6f1f8b9132e15c7949ae49e4ce1f9b2e1a36f84
YSulekha <ysulekha@gmail.com>
2015-04-13 23:54:25 -0700

    Updating

--- src/WordFrequencyCounter.py
+++ src/WordFrequencyCounter.py
@@ -5,6 +5,7 @@ Created on Apr 7, 2015
 '''
 import requests
 from bs4 import BeautifulSoup
+from test.test_iterlen import len
 
 def wordCounter(url):
     word_list = []
@@ -13,10 +14,20 @@ def wordCounter(url):
     soup = BeautifulSoup(text)
     for post_text in soup.findAll('a' ,{'class':'post-title'}):
         content = post_text.string
-        words = content.split()
+        words = content.lower().split()
         for word in words:
-            print(word)
+           # print(word)
             word_list.append(word)
-    
+    list_clean(word_list)
+        
+def list_clean(word_list):
+    clean_List = []
+    symbols = "!@#$%^&*()_+{}:\"?><|';/.,"
+    for word in word_list:
+        for i in range(0,len(symbols)):
+            word = word.replace(symbols[i] , "")
+        if len(word) > 0:
+            clean_List.append(word)
+            print(word)
 wordCounter('https://www.thenewboston.com/forum/')
         
