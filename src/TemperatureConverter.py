3ed43ed281e78fba23ed02d9c167280d4d8481a4
YSulekha <ysulekha@gmail.com>
2015-04-16 00:04:24 -0700

    Submitting

--- src/TemperatureConverter.py
+++ src/TemperatureConverter.py
@@ -7,4 +7,11 @@ def celsius_to_fahr(tc):
     print("Fahr temp "+str(tf))
 def fahr_to_celsius(tf):
     tc = (5/9)(tf-32)
-celsius_to_fahr(5)
+    print("Celsius temp "+str(tc))
+
+t = input("Enter the temperature")
+c = input("Convert to (F)aherenheit or (C)elsius")
+if c is 'F':
+    fahr_to_celsius(t)
+else:
+    celsius_to_fahr(t)
