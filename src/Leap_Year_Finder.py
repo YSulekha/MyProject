dde33ce50300c5021e255801ee2c79d34d5568f9
YSulekha <ysulekha@gmail.com>
2015-04-17 00:14:57 -0700

    Submitting

--- src/Leap_Year_Finder.py
+++ src/Leap_Year_Finder.py
@@ -4,5 +4,11 @@ Created on Apr 16, 2015
 @author: aharyadi
 '''
 def Leapyear_finder(year):
-    print("Leap year")
-Leapyear_finder(1990)
+    if year%4 == 0 and year%100 != 0:
+        print(str(year)+" is a Leap year")
+    elif year%400 == 0:
+        print(str(year)+" is a Leap year")
+    else:
+        print(str(year)+" is not a Leap year")
+        
+Leapyear_finder(2000)
