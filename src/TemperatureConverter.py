d7691ae2afe476cfcdf77324d20b1109a0b9f553
YSulekha <ysulekha@gmail.com>
2015-04-19 22:37:52 -0700

    Submitting

--- src/TemperatureConverter.py
+++ src/TemperatureConverter.py
@@ -2,9 +2,11 @@
 Created on Apr 15, 2015
 
 '''
+
 def celsius_to_fahr(tc):
     tf = (9/5)*tc+32
     print("Fahr temp "+str(tf))
+    
 def fahr_to_celsius(tf):
     tc = (5/9)(tf-32)
     print("Celsius temp "+str(tc))
