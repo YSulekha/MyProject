728bfd44ced48dee7904fde9cedcf86939921c02
YSulekha <ysulekha@gmail.com>
2015-04-25 22:42:28 -0700

    Submitting

--- src/RockScissorPaper.py
+++ src/RockScissorPaper.py
@@ -17,7 +17,10 @@ def compute(user_selection):
     comp_item = random.choice(items)
     print("Human: "+user_selection+"Computer: "+comp_item)
     user_score = 0
+    comp_score = 0
     if user_selection == 'R' and comp_item == 'S':
         user_score = user_score+1
     elif user_selection =='S' and comp_item == 'P':
         user_score = user_score+1
+    else:
+        comp_score = comp_score + 1
