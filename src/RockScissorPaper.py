744da1c17a4daea1e6958f208b3d4fd3fb267246
YSulekha <ysulekha@gmail.com>
2015-04-26 06:34:12 -0700

    Submitting

--- src/RockScissorPaper.py
+++ src/RockScissorPaper.py
@@ -9,7 +9,7 @@ def user_input():
     max_points = input("How many points are required for a win?")
     score = 0
     while score >int(max_points):
-        user_selection = input("Choose (R)ock, (P)aper, or (S)cissors?")
+        user_selection = input("Choose (R)ock (P)aper, or(S)cissors?")
         score = compute(user_selection)
 
 def compute(user_selection):
