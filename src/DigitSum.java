630f43055db4d6b0e84c707b58124f3556ce917e
YSulekha <ysulekha@gmail.com>
2015-05-06 23:42:44 -0700

    Submitting

---
+++ src/DigitSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class DigitSum {
+	public static void main(String args[]){
+		DigitSum s = new DigitSum();
+		Scanner sc = new Scanner(System.in);
+		char num[] = new char[3];
+		s.getDigitSum(num);
+	}
+	public void getDigitSum(char [] num){
+		
+	}
+}
