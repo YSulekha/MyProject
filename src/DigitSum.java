04793ccf47e250d502375f6603aee685588a6063
YSulekha <ysulekha@gmail.com>
2015-05-07 00:46:26 -0700

    Submitting

--- src/DigitSum.java
+++ src/DigitSum.java
@@ -5,10 +5,16 @@ public class DigitSum {
 	public static void main(String args[]){
 		DigitSum s = new DigitSum();
 		Scanner sc = new Scanner(System.in);
-		char num[] = new char[3];
+		System.out.println("Enter number of items");
+		int size = sc.nextInt();
+		int num[] = new int[size];
+		System.out.println("Enter the values");
+		for(int i=0;i<size;i++){
+			num[i] = sc.nextInt();
+		}
 		s.getDigitSum(num);
 	}
-	public void getDigitSum(char [] num){
+	public void getDigitSum(int [] num){
 		
 	}
 }
